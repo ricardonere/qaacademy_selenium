28c0c88539d2fe876bb8fb73a1ef76769162c4f3
Ricardo Nere <ricardonere@gmail.com>
2021-06-26 18:01:57 -0300

    PrimeiraClasse

---
+++ src/test/java/qaacademy_selenium/PrimeiraClasseSelenium.java
@@ -0,0 +1,17 @@
+package qaacademy_selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PrimeiraClasseSelenium {
+	@Test
+	public void testeLogin() {
+		
+		ChromeDriver driver = new ChromeDriver(); 
+		
+		driver.get("https://www.amazon.com.br/");
+		
+	}
+}
+
+	
